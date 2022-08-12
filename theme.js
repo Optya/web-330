59f67b56cb9976d7b5b1236697c80c020b32d3ed
Walter McCue <walter.mccue@gmail.com>
2022-08-11 22:54:53 -0400

    Week 1 Update

---
+++ theme.js
@@ -0,0 +1,33 @@
+/**
+ * Title: theme.js
+ *Author: Walter McCue
+ *Date: 08/12/22
+ *Description: JavaScript to capture, store, and display the light or dark theme that the user selects
+ *References: WEB 300 GitHub; WEB 300 Assign_1;
+ */
+
+/**
+ * Copied the following script from WEB 300 GitHub
+ */
+
+/**
+ * Set the default theme to light if one has not been set in the browsers localStorage
+ */
+ function setDefaultTheme()
+ {
+     const theme = localStorage.getItem("mode") || "light-theme";
+     const iconMode = localStorage.getItem("iconMode") || "fa-toggle-off";
+     const iconText = localStorage.getItem("iconText") || "Light Mode";
+ 
+     document.body.classList.value = theme;
+     document.getElementById("icon-mode").classList.add(iconMode);
+     document.getElementById("icon-text").innerHTML = iconText;
+ }
+ 
+ /**
+  * Set the HTML body to the user's selected theme. If one has not been selected, set the theme to light-theme
+  */
+ function setSelectedTheme()
+ {
+     document.body.classList.value = localStorage.getItem("mode") || "light-theme";
+ }
